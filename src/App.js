49a0962525b1bb48ee7c28fa46616da8cb5cbbb5
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2022-10-23 21:30:02 +0500

    api configuration

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Movie from "./components/Movie";
 
+const FEATURED_MOVIES =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=e30bf5da7850b003dc0bdce0d1596fbb&page=1";
+const SEARCH_MOVIE =
+  "https://api.themoviedb.org/3/search/movie?api_key=e30bf5da7850b003dc0bdce0d1596fbb&language=en-US&query=Avengers&page=1&include_adult=false";
+
 function App() {
+  const [movies, setMovies] = useState();
+
+  useEffect(() => {
+    axios.get(FEATURED_MOVIES).then((res) => {
+      setMovies(res?.data.results);
+    });
+  }, []);
+
+  console.log(movies);
+
   return (
     <div className="app-container">
       <Movie />
